4c6c4cceccb9e2f1d8b4b6638c8cdcc1e45c1db1
Paul-prog44 <saintouenfr@hotmail.com>
2024-09-06 19:14:28 +0200

    Add http codes

--- app.py
+++ app.py
@@ -7,29 +7,36 @@ app = Flask(__name__)
 
 @app.get('/tasks')
 def getAllTasks():
-    return task_db
+    return task_db, 200
 
 @app.get('/tasks/<string:id>')
 def getTasks(id):
-    print(id)
-    return task_db[id]
+    if id in task_db :
+        print(id)
+        return task_db[id], 200
+    else: 
+        print("key not found")
+        return "Cette tâche n'existe pas", 404 
+
 
 @app.post('/tasks')
 def createTask():
     data = request.json
-    print(data)
-    newId = str(uuid.uuid4())
-    task_db[newId] = {
-        newId : {
-            "task_id" : newId, 
-            "name": data['name'],
-            "description": data['description'],
-            "status": data['status'],
-            "created_at" : date.today(),
-            "updated_at" : date.today()
-            }
-        
-    }
+    if not data['name'] or not data['description'] or not data['status'] :
+        return 'missing a value', 400
+    else :
+        newId = str(uuid.uuid4())
+        task_db[newId] = {
+            newId : {
+                "task_id" : newId, 
+                "name": data['name'],
+                "description": data['description'],
+                "status": data['status'], #« TODO », « IN_PROGRESS », « DONE ». Ces valeurs seront transmises par le front
+                "created_at" : date.today(),
+                "updated_at" : date.today()
+                }  
+        }
+
     return task_db[newId] 
 
 @app.delete('/tasks/<string:id>')
